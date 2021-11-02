2c9aa72f36f040e47e05fb6ae81fd7cb6c63273b
Maxim <PogChamp1401@gmail.com>
2021-11-03 00:40:21 +0200

    create db

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(300), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def __repr__(self):
+        return '<Message %r>' % self.id
+
+
+class Tags(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tags = db.Column(db.String(80), nullable=False)
+
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+
+    def __repr__(self):
+        return '<Tags %r>' % self.id
 
 @app.route("/", methods=['GET'])
 def hello_world():
